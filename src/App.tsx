8ca98e895c8ecfe64866f7f3e83a748a202aa5e5
longpd <longpddev@gmail.com>
2022-12-31 15:56:09 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+function App() {
+  const [count] = useState(0);
+
+  return (
+    <div
+      className="sdf sdf sdf s"
+      onClick={() => {}}
+      title="12 "
+    >
+      Hello world
+    </div>
+  );
+}
+
+export default App;
